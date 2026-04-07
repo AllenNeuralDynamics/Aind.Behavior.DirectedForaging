11b93ba96f08f1b41a0750a7029a13e6547d70f7
RoboDoig <erskine94@gmail.com>
2026-04-07 19:29:24 +0100

    Add ffmpeg settings for higher bitrate

--- examples/rig.py
+++ examples/rig.py
@@ -11,9 +11,10 @@ from aind_behavior_directed_foraging.rig import (
 )
 
 FFMPEG_OUTPUT_8BIT = '-vf "scale=out_range=full,setparams=range=full:colorspace=bt709:color_primaries=bt709:color_trc=linear" -c:v h264_nvenc -pix_fmt yuv420p -color_range 2 -colorspace bt709 -color_trc linear -tune hq -preset p3 -rc vbr -cq 18 -b:v 0M -metadata author="Allen Institute for Neural Dynamics" -maxrate 700M -bufsize 350M -f matroska -write_crc32 0'
+FFMPEG_OUTPUT_16BIT = '-vf "format=yuv420p10le,scale=out_range=full,setparams=range=full:colorspace=bt709:color_primaries=bt709:color_trc=linear" -c:v hevc_nvenc -pix_fmt p010le -color_range 2 -colorspace bt709 -color_trc linear -tune hq -preset p4 -rc vbr -cq 12 -b:v 0M -metadata author="Allen Institute for Neural Dynamics" -maxrate 700M -bufsize 350M -f matroska -write_crc32 0'
 FFMPEG_INPUT = "-colorspace bt709 -color_primaries bt709 -color_range 2 -color_trc linear"
 
-video_writer = cameras.VideoWriterFfmpeg(frame_rate=60, container_extension="mp4", output_arguments=FFMPEG_OUTPUT_8BIT, input_arguments=FFMPEG_INPUT)
+video_writer = cameras.VideoWriterFfmpeg(frame_rate=60, container_extension="mp4", output_arguments=FFMPEG_OUTPUT_16BIT, input_arguments=FFMPEG_INPUT)
 
 rig = AindBehaviorDirectedForagingRig(
     computer_name="TestRigComputer", 
